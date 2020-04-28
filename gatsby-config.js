3126a390b1db5aad9d1c4705afdc509b9e667295
justinmcnary <justinmcnary@yahoo.com>
2020-04-28 09:17:34 -0400

    playing with graphql metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog based off of Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Frontend Masters Gatsby Workshop`,
+    description: `A site from a full day Gatsby workshop`,
+    author: `JMC`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
