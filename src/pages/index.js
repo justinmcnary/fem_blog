af86be4cf6374acd707d7eb64d6ae42c920be789
justinmcnary <justinmcnary@yahoo.com>
2020-05-01 15:25:40 -0400

    creating multiple posts with mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/PostPreview';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Charlotte!</p>
-    <Link to="/about/">Learn about me! &rarr; </Link>
-  </Layout>
-);
+const IndexPage = () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Charlotte!</p>
+      <Link to="/about/">Learn about me! &rarr; </Link>
+
+      <h2>Read my Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
 
 export default IndexPage;
